99743a5289f490c87c4aa87b73f97e79c7cac77d
Your Name <you@example.com>
2025-10-03 18:19:45 +0300

    modified global.css and app

--- fruitguard/src/app/page.tsx
+++ fruitguard/src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import Register from "./Register/page";
-import Login from "./Login/page";
 
 export default function Home() {
   return (
     <div>
       <Register/>
-      <Login/>
     </div>
   );
 }

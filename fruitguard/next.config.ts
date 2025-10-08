4caf59c8b72b279ae2aba1e35b0b62151047c324
joanitaawinjo <awinjojoanita@gmail.com>
2025-10-08 08:15:38 +0300

    Update next.config.ts

--- fruitguard/next.config.ts
+++ fruitguard/next.config.ts
@@ -1,12 +1,12 @@
-// next.config.js
+
 module.exports = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'fruitguard-3b97d3adfdf5.herokuapp.com',
-        port: '', // optional, leave empty for default
-        pathname: '/media/**', // allows any path under /media
+        port: '', 
+        pathname: '/media/**',
       },
     ],
   },
